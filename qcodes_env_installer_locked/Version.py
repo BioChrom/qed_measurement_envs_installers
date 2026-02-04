ca4625f3c88eed16d23da17ca9dfa69f08f2027e
EricJutzi <ericjutzi@gmx.ch>
2026-02-04 15:04:30 +0100

    remove unnecessary duplicate files from qcodes_env_installer_locked

--- qcodes_env_installer_locked/Version.py
+++
@@ -1,2 +0,0 @@
-# Copyright 2014-2021 Keysight Technologies
-__version__ = "1.8.5"
--- qcodes_env_installer_locked/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting QCoDeS + Labber installer...
-echo.
-
-powershell -ExecutionPolicy Bypass -File "%~dp0power_shell_script.ps1"
-
-echo.
-echo Installer finished.
-pause
